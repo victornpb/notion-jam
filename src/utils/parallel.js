386bf2ae52a96a3c93be3db1b9fd45d0de43d710
victornpb <3372598+victornpb@users.noreply.github.com>
2023-04-02 00:59:03 -0300

    error handling

--- src/utils/parallel.js
+++ src/utils/parallel.js
@@ -2,18 +2,25 @@ export default function parallel(items, handler, concurrency) {
   if (!Number.isInteger(concurrency) || concurrency < 1)
     throw new Error('concurrency must be a positive integer greater than 0');
   if (items.length === 0) return Promise.resolve([]);
-  return new Promise((resolve, reject) => {
+  return new Promise((jobResolve, jobReject) => {
     const results = [];
     let i = 0;
 
-    const next = (result) => {
+    const resolve = (result) => {
       results.push(result);
+      next();
+    };
+    const reject = (err) => {
+      jobReject(err);
+    };
+
+    const next = () => {
       if (i < items.length) {
         try {
-          handler(items[i++]).then(next).catch(reject);
+          handler(items[i++]).then(resolve).catch(reject);
         } catch (err) { reject(err); }
       }
-      else if (results.length === items.length) resolve(results);
+      else if (results.length === items.length) jobResolve(results);
     };
 
     for (let x = 0; x < Math.min(concurrency, items.length); x++) {
